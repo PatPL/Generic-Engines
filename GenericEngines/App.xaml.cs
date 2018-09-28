0712646f7fd8a6ce98a77ad50e1edc9be66ada0b
PatPL <patrykwyp@poczta.fm>
2018-09-28 18:26:18 +0200

    Global exception logging

--- GenericEngines/App.xaml.cs
+++ GenericEngines/App.xaml.cs
@@ -5,11 +5,31 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.IO;
 
 namespace GenericEngines {
 	/// <summary>
 	/// Logika interakcji dla klasy App.xaml
 	/// </summary>
 	public partial class App : Application {
+		
+		public App () {
+			AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler (ExceptionLogger);
+		}
+
+		static void ExceptionLogger (object sender, UnhandledExceptionEventArgs eventData) {
+			Exception e = (Exception) eventData.ExceptionObject;
+			if (!File.Exists ("crashError.log")) {
+				File.Create ("crashError.log").Close ();
+			}
+			string output = File.ReadAllText ("crashError.log");
+			output += "==========";
+			output += Environment.NewLine;
+			output += DateTime.Now.ToString ();
+			output += Environment.NewLine;
+			output += e.ToString ();
+			output += Environment.NewLine;
+			File.WriteAllText ("crashError.log", output);
+		}
 	}
 }
