6e75ee59e554f4ad9d4e437c6e11e8f2c0706009
PatPL <patrykwyp@poczta.fm>
2018-10-01 20:22:26 +0200

    fancy electric usage text

--- GenericEngines/FuelRatioList.cs
+++ GenericEngines/FuelRatioList.cs
@@ -10,9 +10,14 @@ namespace GenericEngines {
 
 		public override string ToString () {
 			string output = "";
+			string electricSuffix = "";
 
 			foreach (FuelRatioElement i in base.ToArray ()) {
-				output += $"{i.Ratio.ToString (CultureInfo.InvariantCulture)}:";
+				if (i.Propellant != FuelType.ElectricCharge) {
+					output += $"{i.Ratio.ToString (CultureInfo.InvariantCulture)}:";
+				} else {
+					electricSuffix = $" | Electric: {i.Ratio.ToString (CultureInfo.InvariantCulture)}kW";
+				}
 			}
 
 			output = output.Trim (':');
@@ -21,11 +26,15 @@ namespace GenericEngines {
 				output += " ";
 
 				foreach (FuelRatioElement i in base.ToArray ()) {
-					output += $"{FuelName.Name (i.Propellant)}:";
+					if (i.Propellant != FuelType.ElectricCharge) {
+						output += $"{FuelName.Name (i.Propellant)}:";
+					}
 				}
 
 				output = output.Trim (':');
 			}
+
+			output += electricSuffix;
 
 			return output;
 		}
